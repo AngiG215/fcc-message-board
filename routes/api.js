e69353bb02ccaad4c49d14ffc80ff6d3417dbf4a
Angela <angieadrigarcia572@gmail.com>
2026-02-18 18:59:34 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,20 @@
 'use strict';
 
+// Solo necesitamos el controlador. 
+// Asegúrate de que el archivo exista en la carpeta controllers
+const ThreadController = require('../controllers/threadController');
+
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .post(ThreadController.createThread)
+    .get(ThreadController.getRecentThreads)
+    .put(ThreadController.reportThread)
+    .delete(ThreadController.deleteThread);
     
-  app.route('/api/replies/:board');
-
+  app.route('/api/replies/:board')
+    .post(ThreadController.createReply)
+    .get(ThreadController.getSingleThread) 
+    .put(ThreadController.reportReply)
+    .delete(ThreadController.deleteReply);
 };
